4038b7920d760e34a8984b9f68e3e223551e2b0a
Ta1ly <ta1ly@mail.ustc.edu.cn>
2023-05-24 09:49:58 +0800

    fix for a better data save path

--- llama_preprocess_data.sh
+++ llama_preprocess_data.sh
@@ -1,7 +1,7 @@
 python tools/preprocess_data.py \
-    --input /home/nfs/zzq/data/llama-data-1B/arxiv_sample.jsonl \
+    --input /home/nfs/zzq/data/llama-data/arxiv_sample.jsonl \
     --tokenizer-type SentencePieceTokenizer \
     --tokenizer-model llama_tokenizer.model \
     --workers 80 \
     --chunk-size 1 \
-    --output-prefix llama-dataset 
+    --output-prefix data/llama-dataset/arxiv_sample
